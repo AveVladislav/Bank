d1febe0d207745084384b44bca240a940c977346
AveVladislav <Vladislav_king_96@mail.ru>
2016-11-23 02:04:51 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
-
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;  
-var bank = [];
-var bankexx = []; 
+var bank = []; 
 var bankNextId = 1;
 
 app.use(bodyParser.json());
@@ -16,14 +14,14 @@ app.get('/', function(req, res){
 }); 
 
 // GET /bank
-app.get('/bank', function(req, res){
+app.get('/bank', function(req, res){   
 	res.json(bank);
 });
 
 // GET /bank/:id 
 app.get('/bank/:id', function(req, res){    
 	var bankId = parseInt(req.params.id, 10);
-	var matchedBank = _.findWhere(bankexx, {id: bankId});  
+	var matchedBank = _.findWhere(bank, {id: bankId});  
 
 	if(matchedBank){ 
 		res.json(matchedBank);
@@ -44,17 +42,15 @@ app.post('/bank', function(req, res){
 
 		|| body.money.length===0 || !_.isNumber(body.money) || body.percent_for_year.length===0 || !_.isNumber(body.percent_for_year) || !_.isNumber(body.period_day) || body.period_day.length===0){        
 		 
-		return res.status(400).send();  
+		return res.status(400).send();   
 	}
 
-	body.name_client = body.name_client.trim();            
+	body.name_client = body.name_client.trim();             
 	body.id = bankNextId++;
  
-	bank.push(body.name_client + ', your id is ' + body.id + ' and potencial sum on account after ' + body.period_day + ' days = ' 
-		+ (Math.round(body.money+body.money*body.percent_for_year*body.period_day/365/100)));         
- 
-	bankexx.push(body); 
+	body.potencial_sum = Math.round(body.money+body.money*body.percent_for_year*body.period_day/365/100);  
 
+	bank.push(body);         
 	res.json(body.name_client + ', your id is ' + body.id + ' and potencial sum on account after ' + body.period_day + ' days = ' 
 		+ (Math.round(body.money+body.money*body.percent_for_year*body.period_day/365/100)));    
 		
@@ -62,14 +58,14 @@ app.post('/bank', function(req, res){
 
 
 //Delete 
-app.delete('/bank/:id', function(req,res){    
+app.delete('/bank/:id', function(req,res){     
 	var bankId = parseInt(req.params.id,10); 
-	var matchedBank = _.findWhere(bankexx, {id: bankId});  
+	var matchedBank = _.findWhere(bank, {id: bankId});  
 
 	if(!matchedBank){  
 		res.status(404).json({"error": "no account found with that id"});   
 	}else{
-		bankexx = _.without(bankexx, matchedBank); 
+		bank = _.without(bank, matchedBank); 
 		res.json(matchedBank);
 	}
 });
